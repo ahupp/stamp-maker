cdb4beac7395ca6d632be59659882d6437436965
Adam Hupp <adam@hupp.org>
2020-12-11 10:48:56 -0800

    make it simpler

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use image::{io::Reader as ImageReader};
+use image::{ImageBuffer, io::Reader as ImageReader};
 use std::env;
 use std::cmp;
 use std::collections::HashMap;
-use image::{Pixel, Luma};
+use image::{Pixel, Luma, imageops, GenericImageView};
 
 #[derive(Eq, PartialEq, Hash)]
 struct Vert {
@@ -19,98 +19,79 @@ impl Vert {
     }
 }
 
-fn main()  -> Result<(), std::io::Error> {
-
-    let file = env::args().nth(1).unwrap();
+fn read_image(file: &str) -> Result<ImageBuffer<Luma<u8>, Vec<u8>>, std::io::Error> {
     // TODO: not sure how to handle decode error
     let img = ImageReader::open(file)?.decode().unwrap();
 
-    let imgn = img.into_luma8();
+    let luma = img.into_luma8();
+    return Ok(luma);
+}
 
-    let (xd, yd) = imgn.dimensions();
 
-    let max_edge = 50.0;
+fn main()  -> Result<(), std::io::Error> {
+
+    let file = env::args().nth(1).unwrap();
+
+    let img = read_image(&file)?;
+
+    let (oxd, oyd) = img.dimensions();
+    let img = imageops::resize(&img, oxd*2, oyd*2, image::imageops::FilterType::Lanczos3);
+    let (xd, yd) = img.dimensions();
+
+    let max_edge = 20.0;
 
     let maxdim = cmp::max(xd, yd) as f64;
     let scale : f64 = max_edge / maxdim;
+
+
+    let base_height = 1.0;
+
+    let pix_to_z = |x: u32, y: u32| {
+        let value = img.get_pixel(x, y).channels()[0];
+        if value != 0 {
+            base_height
+        } else {
+            2.0
+        }
+    };
 
     let mut vert_to_idx = HashMap::new();
 
     // OBJ file verticies start at 1
     let mut vert_ctr= 1;
 
-    let mut vert_of = |x: u32, y: u32, z: f64| {
+    let mut vert_of = |x: u32, y: u32| {
+        let z = pix_to_z(x, y);
         let v = Vert::new(x as f64 * scale, y as f64 * scale, z);
         let idx = vert_to_idx.entry(v).or_insert_with(||{
+            let cur = vert_ctr;
             vert_ctr += 1;
-            vert_ctr
+            cur
         });
         return *idx;
     };
 
-
     let mut faces = Vec::new();
 
     let mut add_face = |a: u32, b: u32, c: u32, d: u32| {
-        faces.push((a, b, d));
-        faces.push((b, c, d));
+        faces.push((a, b, c));
+        faces.push((c, b, d));
     };
-
     for y in 0..yd-1 {
         for x in 0..xd-1 {
-            let px = imgn.get_pixel(x, y);
-
-            let pix_to_z = |px: &Luma<u8>| {
-                // TODO: select height
-                let value = px.channels()[0];
-                return 1.0;
-                if value == 0 {
-                    4.0
-                } else {
-                    1.0
-                }
-            };
-
-            let z = pix_to_z(px);
-
             add_face(
-                vert_of(x, y, z), vert_of(x, y+1, z),
-                vert_of(x+1, y+1, z), vert_of(x, y+1, z)
-            );
-
-            // check face below
-
-            let pxb = imgn.get_pixel(x, y + 1);
-
-            if px != pxb && false {
-                let zb = pix_to_z(pxb);
-                let (top, bot) = if z > zb {
-                    (z, zb)
-                } else {
-                    (zb, z)
-                };
-
-                add_face(
-                    vert_of(x, y + 1, z), vert_of(x + 1, y + 1, z),
-                    vert_of(x + 1, y + 1, zb), vert_of(x, y + 1, z)
-                );
-            }
-
-            let pxn = imgn.get_pixel(x + 1, y);
-            if px != pxn && false {
-                let zn = pix_to_z(pxn);
-                add_face(
-                    vert_of(x + 1, y, z), vert_of(x + 1, y, zn),
-                    vert_of(x + 1, y + 1, zn), vert_of(x + 1, y + 1, z)
-                );
-            }
+                vert_of(x, y), vert_of(x + 1, y),
+                vert_of(x, y+1), vert_of(x + 1, y + 1),
+            )
         }
     }
+
+
 
     let mut verts : Vec<(Vert, u32)> = vert_to_idx.into_iter().collect();
     verts.sort_by_key(|(_, idx)| *idx);
 
-    println!("g Object001");
+    println!("g stamp");
 
     for (vert, _) in &verts {
         let (x, y, z) = vert.to_float();
