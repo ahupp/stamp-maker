32b865a285e3b883541eaeb234c954996a036121
Adam Hupp <adam@hupp.org>
2020-12-11 16:00:42 -0800

    simplify

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,80 @@
 use image::{ImageBuffer, io::Reader as ImageReader};
 use std::env;
 use std::cmp;
-use std::collections::HashMap;
-use image::{Pixel, Luma, imageops, GenericImageView};
+use std::collections::{HashMap, HashSet};
+use image::{Pixel, Luma, imageops};
 
-#[derive(Eq, PartialEq, Hash)]
+
+#[derive(Clone, Copy, Debug)]
 struct Vert {
-    v: (u64, u64, u64)
+    x: f64,
+    y: f64,
+    z: f64,
 }
 
-impl Vert {
-    fn new(x: f64, y: f64, z: f64) -> Vert {
-        Vert {v: (x.to_bits(), y.to_bits(), z.to_bits())}
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+struct VertIdx {
+    idx: u32,
+}
+
+impl VertIdx {
+    fn new(idx: u32) -> VertIdx {
+        VertIdx {idx}
+    }
+}
+
+struct Face {
+    vert: Vec<VertIdx>,
+}
+
+impl Face {
+    fn tri(a: VertIdx, b: VertIdx, c: VertIdx) -> Face {
+        Face {vert: vec![a, b, c]}
+    }
+}
+
+struct Mesh {
+    verts: Vec<Vert>,
+    faces: HashMap<u32, Face>,
+    index: MeshIndex,
+}
+
+struct MeshIndex {
+    vert_to_face: HashMap<VertIdx, Vec<u32>>,
+    face_to_vert: HashMap<u32, Vec<VertIdx>>,
+}
+
+impl Mesh {
+    fn new() -> Mesh {
+        Mesh {verts: Vec::new(), faces: HashMap::new(),
+            index: MeshIndex {face_to_vert: HashMap::new(), vert_to_face: HashMap::new()}}
     }
 
-    fn to_float(&self) -> (f64, f64, f64) {
-        (f64::from_bits(self.v.0), f64::from_bits(self.v.1), f64::from_bits(self.v.2))
+    fn add_vert(&mut self, x: f64, y: f64, z: f64) -> VertIdx {
+        self.verts.push(Vert {x, y, z});
+        return VertIdx {idx: (self.verts.len() - 1) as u32};
+    }
+
+    fn add_face(&mut self, face: Face) -> u32 {
+        let idx : u32 = self.faces.len() as u32;
+        self.index.face_to_vert.insert(idx, face.vert.clone());
+        for v in &face.vert {
+            self.index.vert_to_face.entry(*v).or_insert_with(|| Vec::new()).push(idx);
+        }
+        self.faces.insert(idx, face);
+        return idx
+    }
+
+    fn print_obj(&self) {
+        println!("g stamp");
+
+        for v in &self.verts {
+            println!("v {} {} {}", v.x, v.y, v.z)
+        }
+
+        for (_, f) in &self.faces {
+            println!("f {} {} {}", f.vert[0].idx + 1, f.vert[1].idx + 1, f.vert[2].idx + 1);
+        }
     }
 }
 
@@ -27,15 +86,12 @@ fn read_image(file: &str) -> Result<ImageBuffer<Luma<u8>, Vec<u8>>, std::io::Err
     return Ok(luma);
 }
 
-
 fn main()  -> Result<(), std::io::Error> {
 
     let file = env::args().nth(1).unwrap();
 
     let img = read_image(&file)?;
 
-    let (oxd, oyd) = img.dimensions();
-    let img = imageops::resize(&img, oxd*2, oyd*2, image::imageops::FilterType::Lanczos3);
     let (xd, yd) = img.dimensions();
 
     let max_edge = 20.0;
@@ -43,64 +99,38 @@ fn main()  -> Result<(), std::io::Error> {
     let maxdim = cmp::max(xd, yd) as f64;
     let scale : f64 = max_edge / maxdim;
 
-
-    let base_height = 1.0;
-
     let pix_to_z = |x: u32, y: u32| {
         let value = img.get_pixel(x, y).channels()[0];
         if value != 0 {
-            base_height
-        } else {
             2.0
+        } else {
+            1.0
         }
     };
 
-    let mut vert_to_idx = HashMap::new();
+    let mut mesh = Mesh::new();
 
-    // OBJ file verticies start at 1
-    let mut vert_ctr= 1;
+    for y in 0..yd {
+        for x in 0..xd {
+            mesh.add_vert(x as f64 * scale, y as f64 * scale,  pix_to_z(x, y));
+            //mesh.add_vert(x as f64 * scale, y as f64 * scale,  (3.14159*(x as f64)/50.).sin());
+        }
+    }
 
-    let mut vert_of = |x: u32, y: u32| {
-        let z = pix_to_z(x, y);
-        let v = Vert::new(x as f64 * scale, y as f64 * scale, z);
-        let idx = vert_to_idx.entry(v).or_insert_with(||{
-            let cur = vert_ctr;
-            vert_ctr += 1;
-            cur
-        });
-        return *idx;
-    };
+    let idx_of = |x: u32, y: u32| (y*xd + x) as u32;
 
-    let mut faces = Vec::new();
-
-    let mut add_face = |a: u32, b: u32, c: u32, d: u32| {
-        faces.push((a, b, c));
-        faces.push((c, b, d));
-    };
     for y in 0..yd-1 {
         for x in 0..xd-1 {
-            add_face(
-                vert_of(x, y), vert_of(x + 1, y),
-                vert_of(x, y+1), vert_of(x + 1, y + 1),
-            )
+            let a = VertIdx::new(idx_of(x, y));
+            let b = VertIdx::new(idx_of(x + 1, y));
+            let c = VertIdx::new(idx_of(x, y + 1));
+            let d = VertIdx::new(idx_of(x + 1, y + 1));
+            mesh.add_face(Face::tri(a, b, c));
+            mesh.add_face(Face::tri(c, b, d));
         }
     }
 
-
-
-    let mut verts : Vec<(Vert, u32)> = vert_to_idx.into_iter().collect();
-    verts.sort_by_key(|(_, idx)| *idx);
-
-    println!("g stamp");
-
-    for (vert, _) in &verts {
-        let (x, y, z) = vert.to_float();
-        println!("v {} {} {}", x, y, z);
-    }
-
-    for (a, b, c) in faces {
-        println!("f {} {} {}", a, b, c);
-    }
+    mesh.print_obj();
 
     return Ok(());
 }
