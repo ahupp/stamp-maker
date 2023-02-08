0898575e291db9e949833e82f45c3ed9d8583bf9
Adam Hupp <adam@hupp.org>
2023-02-07 19:55:52 -0800

    remove optional and unused wee_alloc to silence dependabot

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,10 +1,6 @@
 
 use wasm_bindgen::prelude::*;
 
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
